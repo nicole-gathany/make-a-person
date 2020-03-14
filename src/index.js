6ae659a061d16347d1d837793b6fca3fab51836f
Nicole Gathany <nicole.gathany@gmail.com>
2020-03-14 08:52:49 -0400

    get the first and last name
    added two functions

--- src/index.js
+++ src/index.js
@@ -7,3 +7,33 @@
 // setLastName(last)
 // setFullName(firstAndLast)
 // Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+// running tests
+// Object.keys(bob).length should return 6.
+// bob.getFirstName() should return "Bob".
+// bob.getLastName() should return "Ross".
+// bob.getFullName() should return "Haskell Ross" after bob.setFirstName("Haskell").
+// bob.getFullName() should return "Haskell Curry" after bob.setLastName("Curry").
+// bob.getFullName() should return "Haskell Curry" after bob.setFullName("Haskell Curry").
+// bob.getFirstName() should return "Haskell" after bob.setFullName("Haskell Curry").
+// bob.getLastName() should return "Curry" after bob.setFullName("Haskell Curry").
+// tests completed
+
+var Person = function(firstAndLast) {
+  // Complete the method below and implement the others similarly
+  let nameArr = firstAndLast.split(" ");
+  console.log(nameArr);
+  this.getFullName = function() {
+    return firstAndLast;
+  };
+  this.getFirstName = function() {
+    return nameArr[0];
+  };
+  this.getLastName = function() {
+    return nameArr[1];
+  };
+};
+
+var bob = new Person("Bob Ross");
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
+console.log(bob.getLastName());
